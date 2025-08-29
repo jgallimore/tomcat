912fba04aff63bbe10245314a468532a8ac95c45
Mark Thomas <markt@apache.org>
2025-08-29 14:42:14 +0100

    Fix a couple of issues with QSA/QSD handling and associated tests
    - Original query string was incorrectly retained when there was no new
      query string and QSD was set
    - QSD did not take precedence when QSA and QSD were both set
    
    Rename variables for consistency / ease of maintenance

--- test/org/apache/catalina/startup/TomcatBaseTest.java
+++ test/org/apache/catalina/startup/TomcatBaseTest.java
@@ -562,7 +562,7 @@ public abstract class TomcatBaseTest extends LoggingBaseTest {
                         value.append(";");
                     }
                 }
-                out.println("PARAM/" + name + ": " + value);
+                out.println("PARAM:" + name + ": " + value);
             }
 
             out.println("SESSION-REQUESTED-ID: " +
