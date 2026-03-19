8265e2f6a4bfb08d0117ae766a4a8b1f983e1dfa
Jonathan Gallimore <jon@jrg.me.uk>
2026-03-19 17:31:16 +0000

    Revert "Fix case sensitive handling of the protocol host name"
    This reverts commit 2e039ae0d2ca5d3d3136c603a55c46b6b4c34fab.

--- java/org/apache/tomcat/util/net/AbstractEndpoint.java
+++ java/org/apache/tomcat/util/net/AbstractEndpoint.java
@@ -447,8 +447,7 @@ public abstract class AbstractEndpoint<S,U> {
     public boolean checkSni(String sniHostName, String protocolHostName) {
         return (!strictSni || !isSSLEnabled()
                 || (sniHostName != null && sniHostName.equalsIgnoreCase(protocolHostName))
-                || getSSLHostConfig(sniHostName) == getSSLHostConfig(
-                        protocolHostName != null ? protocolHostName.toLowerCase(Locale.ENGLISH) : null));
+                || getSSLHostConfig(sniHostName) == getSSLHostConfig(protocolHostName));
     }
 
 
